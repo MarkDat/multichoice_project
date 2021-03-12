f3eb737e6a290ba55dedfd985a9d4ea81e613601
Cojol0511 <55191426+Cojol0511@users.noreply.github.com>
2021-03-12 14:34:13 +0700

    edit header

--- src/main/java/com/wild/controller/web/HomeController.java
+++ src/main/java/com/wild/controller/web/HomeController.java
@@ -3,18 +3,13 @@ package com.wild.controller.web;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.wild.daos.impl.ExamDTODao;
-import com.wild.daos.impl.ExamDao;
-import com.wild.daos.impl.GradeDao;
-import com.wild.daos.impl.QuestionDao;
-import com.wild.daos.impl.RankUserDao;
 import com.wild.daos.impl.SubjectDao;
-import com.wild.daos.impl.UserMarkDao;
-import com.wild.models.RankUser;
 import com.wild.models.Subject;
 
 @Controller(value = "homeControllerOfWeb")
@@ -23,37 +18,17 @@ public class HomeController {
 	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
 	public ModelAndView homePage() {
 		ModelAndView mav = new ModelAndView("web/home");
-		QuestionDao qs = new QuestionDao();
-		ExamDTODao exd = new ExamDTODao();
-		ExamDao ex = new ExamDao();
-		// List<Grade> grades = a.getAll();
-		// List<Question> q = qs.findListQuesByIdExam(1L);
-		// List<Exam> q = ex.findExamsBySubjectId(4L);
-
 		SubjectDao sd = new SubjectDao();
-
-		UserMarkDao umd = new UserMarkDao();
-		RankUserDao rud = new RankUserDao();
-		
-//		List<Grade> grades = a.getAll();
-		//List<Question> q = qs.findListQuesByIdExam(1L);
-		//List<Exam> q = ex.findExamsBySubjectId(4L);
-		
-		for (RankUser q : rud.findAll()) {
-			System.out.println(q.getName()+" "+q.getTotalMark());
-		}
-		
-
 		List<Subject> listSup = sd.findAll();
-		GradeDao a = new GradeDao();
-		List<Grade> listGrade = a.getAll();
-		
 		mav.addObject("listSup", listSup);
-		mav.addObject("listGrade", listGrade);
-//		Exam objEx = new Exam(null, 3L, "Đề thi hóa phần hữu cơ");
-//		System.out.println("OK add ? "+ex.addNewExam(objEx));
 		return mav;
 	}
+	@RequestMapping(value = "/grade/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public String method7(@PathVariable("id") int id){
+		return "method7 with id="+id;
+	}
+	
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView loginPage() {
@@ -61,16 +36,12 @@ public class HomeController {
 		return mav;
 	}
 
-	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public ModelAndView registerPage() {
-		ModelAndView mav = new ModelAndView("pages_other/register");
-		return mav;
-	}
 	@RequestMapping(value = "/customer_info", method = RequestMethod.GET)
 	public ModelAndView cusInfoPage() {
 		ModelAndView mav = new ModelAndView("web/customer_info");
 		return mav;
 	}
+
 	@RequestMapping(value = "/edit_password", method = RequestMethod.GET)
 	public ModelAndView editPassPage() {
 		ModelAndView mav = new ModelAndView("web/edit_password");
